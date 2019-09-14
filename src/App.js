1b10908c2252a862202db6c7a45e70f2d2b57568
Cao Qian <talexcao@gmail.com>
2019-09-14 00:07:31 -0700

    components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
+import ComponentHeader from './components/header';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Hello! </h1>
+        <ComponentHeader/>
+        <h1> 何老版! </h1>
       </div>
     );
   }
